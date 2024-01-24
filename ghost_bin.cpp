3ae3b91ce6c99043151c462826afce5b892bbd27
Julianzbk <62032232+Julianzbk@users.noreply.github.com>
2024-01-24 15:28:35 -0800

    Update ghost_bin.cpp

--- ghost_bin.cpp
+++ ghost_bin.cpp
@@ -1,56 +1,83 @@
 #include <iostream>
-#include <string>
 
-struct BinaryString
-{
-    char* first;
-    size_t len;
-};
+bool fixed_width_encoding = true; // Setting false will render message more aesthetic but undecodable.
 
-BinaryString encode(std::istream& input)
-{
-    std::string word;
-    while (input >> word)
-    {
-        for (size_t i = 0; word[i] != '\0'; ++i)
-        {
-            std::string bin_char = to_char(word[i]);
-
-        }
-    }
-}
-
-std::string to_bin(int num)
+std::string num_to_bin(int num)
 {
     std::string bin = "";
     int rem;
-    while (num % 2 != 0)
+    do
     {
         rem = num % 2;
-        bin.append(std::to_string(rem));
+        if (rem == 0)
+            bin.append("o");
+        else
+            bin.append("O");
+        num = num / 2;
+    } while (num > 0);
+    
+    if (fixed_width_encoding) {
+        while (bin.size() < 6)
+            bin.append("o");
     }
 
-    //reverse bin
+    char holder;
+    size_t size = bin.size();
+    for (size_t i = 0; i < size / 2; ++i)
+    {
+        holder = bin[i];
+        bin[i] = bin[size - i - 1];
+        bin[size - i - 1] = holder;
+    }
     return bin;
 }
 
-std::string to_char(char c)
+bool is_symbol(char c)
 {
-    int c_num;
-    if (c >= 65 && c <= 90)
+    return c == 10 || (c >= 32 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || (c >= 123 && c <= 126) && c != ' ';
+}
+
+std::string encode(std::istream& input)
+{
+    std::string line = "";
+    char c;
+    while (input.get(c))
     {
-        c_num = c - 38;
+        //std::cout << "c = " << c << " peek = " << input.peek() << std::endl;
+        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        {
+            int c_num = c;
+            if (c >= 'a' && c <= 'z')
+                c_num = c - 38;
+            else if (c >= 'A' && c <= 'Z')
+                c_num = c - 96;
+
+            line.append(num_to_bin(c_num));
+            char p = input.peek();
+            if (p == 39 && c != ' ') {}
+            else if (is_symbol(p))
+            {
+                line.append("h");
+            }
+        }
+        else
+        {
+            line.append(std::string(1, c));
+        }
     }
-    if (c >= 97 && c <= 122)
-    {
-        c_num = c - 96;
-    }
-    return to_bin(c_num)
+    return line;
 }
 
 
 int main()
 {
-    encode(std::cin);
+    /*
+    for (char c = 60; c < 'z'; ++c)
+    {
+        std::cout << c << " " << char_to_bin(c) << std::endl;
+    }
+    */
+    std::cout << "Enter string. End input with Ctrl-Z." << std::endl;
+    std::cout << encode(std::cin);
     return 0;
 }
